1b8a3d6de64790abacbf9509774bcca8687dd1b3
ionut_manevra <ionu2044@gmail.com>
2024-09-27 12:18:59 +0300

    modified cosCumparaturi

--- src/CosCumparaturi.java
+++ src/CosCumparaturi.java
@@ -2,4 +2,37 @@ public class CosCumparaturi {
     public String produse;
     public int pretCos;
     public int numarProduse;
+
+    CosCumparaturi(String produse,int pretCos,int numarProduse){
+        this.produse=produse;
+        this.pretCos=pretCos;
+        this.numarProduse=numarProduse;
+    }
+
+    public String getProduse() {
+        return produse;
+    }
+
+    public void setProduse(String produse) {
+        this.produse = produse;
+    }
+
+    public int getPretCos() {
+        return pretCos;
+    }
+
+    public void setPretCos(int pretCos) {
+        this.pretCos = pretCos;
+    }
+
+    public int getNumarProduse() {
+        return numarProduse;
+    }
+
+    public void setNumarProduse(int numarProduse) {
+        this.numarProduse = numarProduse;
+    }
+
 }
+
+
